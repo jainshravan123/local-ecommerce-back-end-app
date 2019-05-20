6360e19a5c0f2aef873e5f929f37e919d1bbc285
Sharvan Jain <sharjain@in.ibm.com>
2019-05-20 21:26:05 +0530

    adding console log

--- src/models/db.js
+++ src/models/db.js
@@ -1,5 +1,6 @@
 let mongoose = require('mongoose')
 
+console.log('process.env.MONGODB_URI : ', process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ECommerceAppDb', {useNewUrlParser: true}, (err) => {
     if (!err) {
         console.log('MongoDB connection succeeded.');
